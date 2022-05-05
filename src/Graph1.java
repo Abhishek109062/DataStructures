86bb2a89a7bd84280e89f16d7e4393d33060d17d
Abhishek Kumar <abhi109062@gmail.com>
2022-05-05 14:09:50 +0530

    ff

--- src/Graph1.java
+++ src/Graph1.java
@@ -5,18 +5,22 @@ import java.util.function.BiFunction;
 
 public class Graph1 {
     public static void main(String[] args) {
-        int v=5;
+        int v=7;
         ArrayList<ArrayList <Integer>> adj= new ArrayList<>();
         for(int x=0;x<v;x++)
             adj.add(new ArrayList<Integer>(v));
 
         addEdge(adj,0,1);
         addEdge(adj,0,2);
-        addEdge(adj,2,1);
         addEdge(adj,1,3);
+        addEdge(adj,2,3);
+        addEdge(adj,4,5);
+        addEdge(adj,4,6);
+        addEdge(adj,5,6);
 //
-//        printGraph(adj);
-        BFS(adj,4,0);
+     //printGraph(adj);
+       // BFS(adj,6,0);
+        System.out.println(BFSDis(adj,7));
     }
 
     static void addEdge(ArrayList<ArrayList <Integer>> adj , int u ,int v){
@@ -41,11 +45,40 @@ public class Graph1 {
             int u=q.poll();
             System.out.print(u+" ");
             for(int x:adj.get(u)){
-                if(!visited[v]){
-                    visited[v]=true;
-                    q.add(v);
+                if(!visited[x]){
+                    visited[x]=true;
+                    q.add(x);
                 }
             }
         }
+    }
+
+    //This is used when no source is given and the graph is disconnected
+    static void BFS(ArrayList<ArrayList<Integer>> adj, int v,int s,boolean[] visited){
+        Queue<Integer> q=new LinkedList<>();
+        visited[s]=true;
+        q.add(s);
+        while(!q.isEmpty()){
+            int u=q.poll();
+            System.out.print(u+" ");
+            for(int x:adj.get(u)){
+                if(!visited[x]){
+                    visited[x]=true;
+                    q.add(x);
+                }
+            }
+        }
+    }
+
+    static int BFSDis(ArrayList<ArrayList<Integer>> adj,int v){
+        boolean visited[]=new boolean[v+1];
+        int count=0; //this is used to count the connected components
+        for(int i=0;i<v;i++)
+            if(!visited[i]){
+                BFS(adj,i,i,visited);
+                count++;
+            }
+
+        return count;
     }
 }
