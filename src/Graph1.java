082b030a3238a2cecd854ee1472402c2257c9033
Abhishek Kumar <abhi109062@gmail.com>
2022-05-06 12:20:27 +0530

    prim's algo for minimum spanning tree

--- src/Graph1.java
+++ src/Graph1.java
@@ -1,11 +1,10 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.*;
 import java.util.Queue;
 import java.util.function.BiFunction;
 
 public class Graph1 {
     public static void main(String[] args) {
-        int v=5 ;
+        int v=4 ;
         ArrayList<ArrayList <Integer>> adj= new ArrayList<>();
         for(int x=0;x<v;x++)
             adj.add(new ArrayList<Integer>(v));
@@ -13,7 +12,7 @@ public class Graph1 {
         addEdge(adj,0,1);
         addEdge(adj,0,2);
         addEdge(adj,1,2);
-        addEdge(adj,2,4);
+        addEdge(adj,2,3);
 
 //        addEdge(adj,4,6);
 //        addEdge(adj,5,6);
@@ -24,6 +23,21 @@ public class Graph1 {
         //DFS(adj,v,0);
         //DFS2(adj,v);
         //shortestDis(adj,5,0);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of vertex");
+        int n= sc.nextInt();
+        System.out.println("Enter the number of edges");
+        int e = sc.nextInt();
+        int matrix[][] = new int[n][n];
+        for (int i = 0; i <e; i++) {
+            int v1 = sc.nextInt();
+            int v2 = sc.nextInt();
+            int weight = sc.nextInt();
+            // creation of undirected graph
+            matrix[v1][v2]= weight;
+            matrix[v2][v1] = weight;
+        }
+        prims(matrix);
     }
 
     static void addEdge(ArrayList<ArrayList <Integer>> adj , int u ,int v){
@@ -76,7 +90,7 @@ public class Graph1 {
 
     //Breadth First Search - Used for disconnected graph
     static int BFSDis(ArrayList<ArrayList<Integer>> adj,int v){
-        boolean visited[]=new boolean[v+1];
+        boolean[] visited =new boolean[v+1];
         int count=0; //this is used to count the connected components
         for(int i=0;i<v;i++)
             if(!visited[i]){
@@ -112,7 +126,7 @@ public class Graph1 {
     }
 
     static int DFS2(ArrayList<ArrayList<Integer>> adj,int v){
-        boolean visited[]=new boolean[v];
+        boolean[] visited =new boolean[v];
         int count=0;//used to count the connected graphs
         for(int i=0;i<v;i++){
             if(!visited[i])
@@ -150,4 +164,51 @@ public class Graph1 {
 //        for(int x=0;x<v;x++)
 //            System.out.println(dis[x]);
 //    }
+
+    //Minimum Spanning Tree - Prism's Algorithm
+    public static void prims(int matrix[][])
+    {
+        int n = matrix.length;
+        boolean visited[] = new boolean[n];
+        int parent[] = new int[n];
+        int weight[] = new int[n];
+        // assign the value of source vertex from where you start
+        parent[0] = -1;
+        weight[0] = 0;
+        for (int i = 1; i <n ; i++) {
+            weight[i] = Integer.MAX_VALUE;
+        }
+        for (int i = 0; i <n ; i++) {
+            int minvertex = findMinimum(visited,weight);
+            visited[minvertex]=true;
+            // check the neighbours of the visited vertex
+
+            for (int j = 0; j <n ; j++) {
+                if(matrix[minvertex][j] !=0 && !visited[j])
+                {
+                    if(weight[j]>matrix[minvertex][j])
+                    {
+                        weight[j]= matrix[minvertex][j];
+                        parent[j]= minvertex;
+                    }
+                }
+            }
+        }
+        // printing the minimum spanning tree
+        for (int i = 0; i <n ; i++){
+
+            System.out.println(i+" "+parent[i]+" "+weight[i]);
+
+        }
+    }
+    // method to find the minimum vertex
+    private static int findMinimum(boolean[] visited, int[] weight) {
+        int minvertex = -1;
+        for (int i = 0; i <visited.length ; i++) {
+            if(!visited[i] && (minvertex==-1 || weight[i]<weight[minvertex])){
+                minvertex = i;
+            }
+        }
+        return minvertex;
+    }
 }
